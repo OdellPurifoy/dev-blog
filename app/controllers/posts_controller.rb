89c533491e0b051c54cfd5610c080aa61b4e57b6
Odell <odell.purifoy@gmail.com>
2018-07-30 19:10:57 -0400

    Initial dev blog commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to (posts_path)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      redirect_to 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    paras.require(:post).permit(:title, :body)
+  end
+end
